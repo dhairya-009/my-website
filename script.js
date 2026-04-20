7081e9f800c882a3d3c8fa69d15ee5d99b00848c
DHAIRYA <iamdhairya13@gmail.com>
2026-04-20 09:09:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function showMessage() {
+    alert("🔥 Welcome to my advanced website!");
+}
+
 function toggleMode() {
     document.body.classList.toggle("light-mode");
 
@@ -9,3 +13,17 @@ function toggleMode() {
         btn.innerText = "🌙";
     }
 }
+
+// Typing Effect
+let text = "Just a little project, for fun 🚀";
+let i = 0;
+
+function typeEffect() {
+    if (i < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeEffect, 50);
+    }
+}
+
+typeEffect();
