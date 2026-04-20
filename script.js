2159c7f376a86a9c2b5de74f11a784f782a200a3
DHAIRYA <iamdhairya13@gmail.com>
2026-04-20 08:59:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-alert("JS loaded");
-
-function toggleMode() {
-    document.body.classList.toggle("light-mode");
-
-    let btn = document.getElementById("modeBtn");
-
-    if (document.body.classList.contains("light-mode")) {
-        btn.innerText = "🌞";
-    } else {
-        btn.innerText = "🌙";
-    }
+function showMessage() {
+    alert("🔥 Welcome to my advanced website!");
 }
